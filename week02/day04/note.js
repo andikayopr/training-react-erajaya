d0b96d0604bef9bda8fe4470c08f40b01efdaf0a
Andika Yopi Pr <ayprihartomo@gmail.com>
2022-11-14 02:06:50 +0700

    Week 02 day 04 - React app installation

---
+++ week02/day04/note.js
@@ -0,0 +1,17 @@
+// React using old javascript before ES6
+// Not understand modern syntax like arrow function dll
+// So react using babel to handle this
+// babel is transpiler
+// webpack membundle semua kedalam suatu file html
+// html , head, body, footer, dll = Document object model (DOM)
+// Kelemahan dom ketika ada perubahan harus di load ulang
+// React using virtual DOM
+// Ketika update hanya sebagian saja tidak semua di load ulang
+// install library harus didalam folder yang mempunyai module
+
+// Ketika bekerja kolaborasi dengan yang lain
+// pastikan gunakan satu package manager (yarn, npm)
+// jangan beda2 
+
+// dependencies akan dibawa ke build ke production
+// dev dependencies hanya digunakan di local development
